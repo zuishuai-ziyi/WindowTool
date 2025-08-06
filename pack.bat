0c0c25c770b0689b8a38520e9dc5420b5c40c34e
zuishuai-ziyi <12794591@qq.com>
2025-08-06 11:37:07 +0800

    bugfix(pack.bat): 修复 pack.bat 中的资源文件路径错误

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Packaging...
 cd /d "%~dp0"
-pyinstaller main.py --noconsole --add-data "data;data" -i "./data\icon\window.png" --add-binary "./uia.dll;."
+pyinstaller main.py --noconsole --add-data "data;data" -i ".\data\icon\window.png" --add-binary ".\UIAccess.dll;."
 echo Packaging completed.
 timeout 5
